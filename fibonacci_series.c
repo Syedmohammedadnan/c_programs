897cf5ae0e79a163ef25b7098bef5445a57c0b31
Syed Mohammed Adnan <syedadnan007.sa@gmail.com>
2022-12-20 11:48:46 +0530

    Code reduction

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-    int fubanaci(int num){
-    int p_val = 1,c_val = 1,temp;
-    printf("%d ",c_val);
-        printf("%d ",p_val);
-    while (c_val < num){
+int fubanaci(int num) {
+    int p_val = 1, c_val = 1, temp;
+    printf("%d ", c_val);
+    printf("%d ", p_val);
+    while (c_val < num) {
         temp = p_val;
         p_val = c_val;
         c_val = temp;
         c_val = p_val + c_val;
-        printf("%d ",c_val);
-        
+        if (c_val > num) {
+            return 0;
         }
-return 0;
+        printf("%d ", c_val);
+
+    }
+    return 0;
 
 }
 
 
 int main()
 {
-    int check,diviser;
-    int num,power,result=0;
+    int check, diviser;
+    int num, power, result = 0;
     printf("enter the input number\n");
-    scanf("%d",&num);
-    printf("enter the power\n");
-    scanf("%d",&power);
+    scanf("%d", &num);
+
     fubanaci(num);
     return 0;
 }
